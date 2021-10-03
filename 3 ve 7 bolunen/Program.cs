df7dd33fc7250779c3ba6aa011e3815024092028
murad <murad@DESKTOP-TQKOADC>
2021-10-04 00:00:40 +0400

    Add project files.

---
+++ 3 ve 7 bolunen/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace tasklar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            int n = 21;
+
+            if (n % 3 == 0 && n % 7 == 0)
+
+            {
+                Console.WriteLine(true);
+            }
+            
+            else
+            {
+                
+                Console.WriteLine(false);
+ 
+            }
+            #endregion
+            #region
+            n və m(n<m) ədədləri arasında neçə tək ədəd olduğunu tapın
+            int n = 20;
+            int m = 60;
+            int count = 0;
+            for (int i = n; i <= m; i++)
+            {
+                if (i % 3 == 0)
+
+                {
+
+                    count++;
+
+                }
+            }
+            Console.WriteLine(count);
+            #regionend
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
